7bab4fb1ca163e2bfca6dd51b4f0bc8702c22174
Santiago Correa <scorreac1@eafit.edu.co>
2025-01-11 20:18:41 -0500

    Use of fetch instead of axios

--- src/app/boxClient.ts
+++ src/app/boxClient.ts
@@ -1,18 +1,22 @@
-import axios from 'axios'; // HTTP requests
-
 export const getFolderItems = async (accessToken: string, folderId: string) => {
   try {
-    const response = await axios.get(`https://api.box.com/2.0/folders/${folderId}/items`, {
+    const response = await fetch(`https://api.box.com/2.0/folders/${folderId}/items?fields=id,name,type`, {
+      method: 'GET',
       headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      params: {
-        fields: 'id,name,type',
-      },
+        'Authorization': `Bearer ${accessToken}`
+      }
     });
-    return response.data.entries;
+
+    if (!response.ok) {
+      throw new Error(`Error fetching folder items: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data.entries;
   } catch (error) {
     console.error('Error fetching folder items:', error);
     throw error; 
   }
 };
+
+
